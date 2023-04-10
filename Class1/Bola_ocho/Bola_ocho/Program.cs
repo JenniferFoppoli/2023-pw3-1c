54661d899c1e1e921b7381ba1db1bcfe11e709a1
Jennifer Foppoli <jenniferfoppoli@hotmail.com>
2023-04-10 14:59:37 -0300

    falta validar que la pregunta no sea vacia

---
+++ Class1/Bola_ocho/Bola_ocho/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+ 
+public class BolaOcho
+{
+    public static void Main()
+    {
+        Console.WriteLine("ingrese una pregunta que pueda responderse con si o no");
+        string pregunta = Console.ReadLine();
+
+        List<string> respuestas = new List<string>(){ "Sí, definitivamente", "Es cierto", "Es decididamente así", 
+            "Sin lugar a dudas","Puedes confiar en ello","Como yo lo veo, sí", "Lo más probable"," Perspectiva buena"," Sí",
+            "Las señales apuntan a que sí"," Respuesta confusa"," vuelve a intentarlo"," Vuelve a preguntar más tarde",
+            "Mejor no decirte ahora"," No se puede predecir ahora", "Concéntrate y vuelve a preguntar","No cuentes con ello",
+            " Mi respuesta es no"," Mis fuentes dicen que no"," Las perspectivas no son muy buenas", " Muy dudoso"};
+
+
+
+
+        Random rnd = new Random();
+        int indiceAleatorio = rnd.Next(respuestas.Count);
+        string random = respuestas[indiceAleatorio];
+
+        Console.WriteLine(random);
+    }
+}
